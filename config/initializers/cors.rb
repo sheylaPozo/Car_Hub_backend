e0a86a9adce2d41a58881f95c91ccdb6d0e22e32
eroiyo <eroiyo@outlook.es>
2021-12-10 18:25:05 -0400

    Configure Rack Middleware, Add needed gems

---
+++ config/initializers/cors.rb
@@ -0,0 +1,21 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+
+    allow do
+
+      origins '*'
+
+      resource(
+
+       '*',
+
+       headers: :any,
+
+       expose: ["Authorization"],
+
+       methods: [:get, :patch, :put, :delete, :post, :options, :show]
+
+      )
+
+    end
+
+  end
