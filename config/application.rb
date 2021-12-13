3d6881954d5c8fe2d378591b827f78989df42aa2
eroiyo <eroiyo@outlook.es>
2021-12-13 06:29:49 -0400

    config.assets.initialize_on_precompile = true

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,5 @@ module DeviseJwtApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.initialize_on_precompile = false
   end
 end
