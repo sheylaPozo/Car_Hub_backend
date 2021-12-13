5461a38eb1b87c8bde48fcdbf40c1e4d4cb5fcb3
eroiyo <eroiyo@outlook.es>
2021-12-13 06:47:08 -0400

    using credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@
 Devise.setup do |config|
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise[ENV["API_KEY"]]
+    jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
   end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
