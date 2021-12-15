37e9379ee08bb632e4de0e3b4a415a738d876c59
eroiyo <eroiyo@outlook.es>
2021-12-15 06:41:57 -0400

    fix linters

--- app/controllers/reserved_controller.rb
+++ app/controllers/reserved_controller.rb
@@ -1,41 +1,41 @@
 # frozen_string_literal: true
 
 class ReservedController < ApplicationController
-    def index
-        if user_signed_in?
-            render json: current_user.reserved_cars
-        else
-            render json: { message: 'You are not connected' }
-        end
-    end
-
-    def create
-        if user_signed_in?
-            reserved_car = ReservedCar.new
-            reserved_car.user = current_user
-            reserved_car.car = Car.find(params[:car_id])
-            reserved_car.country = params[:country]
-            reserved_car.date = params[:date]
-            if reserved_car.save
-                render json: { message: 'Car Reserved' }
-            else
-                render json: { message: 'Invalid Data' }
-            end
-        else
-            render json: { message: 'You are not connected' }
-        end
-    end
-
-    def delete
-        if user_signed_in?
-            if current_user.reserved_cars.find(params[:reserved_id])
-                current_user.reserved_cars.delete_by(id: params[:reserved_id])
-                render json: { message: 'Reservation Deleted' }
-            else
-                render json: { message: 'Reservation Not Found' }
-            end
-        else
-            render json: { message: 'You are not connected' }
-        end
+  def index
+    if user_signed_in?
+      render json: current_user.reserved_cars
+    else
+      render json: { message: 'You are not connected' }
     end
   end
+
+  def create
+    if user_signed_in?
+      reserved_car = ReservedCar.new
+      reserved_car.user = current_user
+      reserved_car.car = Car.find(params[:car_id])
+      reserved_car.country = params[:country]
+      reserved_car.date = params[:date]
+      if reserved_car.save
+        render json: { message: 'Car Reserved' }
+      else
+        render json: { message: 'Invalid Data' }
+      end
+    else
+      render json: { message: 'You are not connected' }
+    end
+  end
+
+  def delete
+    if user_signed_in?
+      if current_user.reserved_cars.find(params[:reserved_id])
+        current_user.reserved_cars.delete_by(id: params[:reserved_id])
+        render json: { message: 'Reservation Deleted' }
+      else
+        render json: { message: 'Reservation Not Found' }
+      end
+    else
+      render json: { message: 'You are not connected' }
+    end
+  end
+end
--- app/models/reserved_car.rb
+++ app/models/reserved_car.rb
@@ -4,14 +4,12 @@ class ReservedCar < ApplicationRecord
   belongs_to :user
   belongs_to :car
 
-  
   def as_json(_options = {})
     { id: id,
       name: car.name,
       description: car.description,
       image: car.image_url,
       date: date,
-      country: country
-    }
+      country: country }
   end
 end
