2099a53a9edab2fe95737798efa754ced597b28a
eroiyo <eroiyo@outlook.es>
2021-12-20 14:57:27 -0400

    do the headers give more information

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-  
-      resource '*',
-               headers: :any,
-               methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins '*'
+    resource(
+     '*',
+     headers: :any,
+     expose: ["Authorization"],
+     methods: [:get, :patch, :put, :delete, :post, :options, :show]
+    )
   end
+end
